46c2361c022b0e5e887223f81987ef5792f7ed96
Nestorsoiza <nestor.soiza83@gmail.com>
2020-12-05 19:50:53 +0100

    termino clase revolver
    el tambor del revolver se supone que tiene 6 posiciones porque el rango va entre 1 y 6

--- src/game/Revolver.java
+++ src/game/Revolver.java
@@ -1,5 +1,33 @@
 package game;
 
 public class Revolver {
+	private int posicionBalaActual;
+	private int posicionBala;
+	
+	public boolean disparar() {
+		boolean exito = false;
+		if (posicionBalaActual == posicionBala) {
+			exito = true;
+		}
+		siguienteBala();
+		return exito;
+	}
 
+	public void siguienteBala() {
+		if (posicionBalaActual == 6) {
+			posicionBalaActual = 1;
+		} else {
+			posicionBalaActual ++;
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Revolver [posicionBalaActual = " + posicionBalaActual 
+				+ ", posicionBala = " + posicionBala + "]";
+	}
+		
 }
+
+
